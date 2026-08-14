c7e668a7ea57b6060f048a20d024ce4544a21809
panhalkarpratiksha7-jpg <panhalkarpratiksha7@gmail.com>
2026-08-14 10:16:26 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,1 +1,30 @@
+public class App {
 
+    public static boolean isPositive(int num) {
+        return num >= 0;
+    }
+
+    public static int add(int a, int b) {
+        if (a < 0 || b < 0) {
+            System.out.println("Negative number found. Addition not performed.");
+            return 0;
+        }
+
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        if (isPositive(a) && isPositive(b)) {
+            int result = add(a, b);
+            System.out.println("Addition = " + result);
+            System.out.println("Result: true");
+        } else {
+            System.out.println("Negative number found.");
+            System.out.println("Result: false");
+        }
+    }
+}
