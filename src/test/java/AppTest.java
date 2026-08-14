5b9dc46ca3f17006dd6b5fa9b028da0e53566014
panhalkarpratiksha7-jpg <panhalkarpratiksha7@gmail.com>
2026-08-14 10:23:40 +0530

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,1 +1,24 @@
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testPositiveNumbers() {
+        int result = App.add(10, 20);
+
+        assertEquals(30, result);
+        assertTrue(App.isPositive(10));
+        assertTrue(App.isPositive(20));
+    }
+
+    @Test
+    void testNegativeNumber() {
+        int a = 10;
+        int b = -20;
+
+        assertTrue(!App.isPositive(b));
+    }
+}
